43f668a3a7a9d48552ad64d39ce5ca508e07a34f
Wepea0 <92167178+Wepea0@users.noreply.github.com>
2024-01-26 23:30:30 +0000

    Added static site file

---
+++ SpotifyCVApp.js
@@ -0,0 +1,13 @@
+const usernameEl = document.getElementById("Username");
+const experienceEl = document.getElementById("Recent_Experience");
+const favouriteSongEl = document.getElementById("Favourite_Song");
+const gpaEl = document.getElementById("Gpa");
+const submitEl = document.getElementById("Submit");
+
+submitEl.onclick = function changeInfo(){
+const newCVName = document.getElementById("Experience").value;
+console.log(newCVName);
+usernameEl.innerHTML = "Fhfhd";
+experienceEl.innerHTML = "BOBBBBBBBBB";
+
+}
